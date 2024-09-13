bdc1b2f0460a270b928eed8faef1421385084057
Yashar <yashar22mo@gmail.com>
2024-09-13 15:21:31 +0330

    git init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+// import { routes } from "./Routes";
+// import { useRoutes } from "react-router-dom";
+
+export default function App() {
+	// let router = useRoutes(routes);
+
+	return (
+		<>
+			<div className="container">
+			</div>
+		</>
+	);
+}
---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+// import Home from "./pages/Home/Home";
+// import Users from "./pages/Users/Users";
+// import NewUser from "./pages/NewUser/NewUser";
+// import Products from "./pages/Products/Products";
+// import Product from "./pages/Product/Product";
+
+// export let routes = [
+// 	{ path: "/", element: <Home /> },
+// 	{ path: "/home", element: <Home /> },
+// 	{ path: "/users", element: <Users /> },
+// 	{ path: "/newuser", element: <NewUser /> },
+// 	{ path: "/products", element: <Products /> },
+// 	{ path: "/products/:id", element: <Product /> },
+// ];
