0abd16b059b75d6b769285aaff62bf0bce44e815
Yashar <yashar22mo@gmail.com>
2024-09-14 23:16:50 +0330

    change theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './Header.css'
-import Navbar from '../Navbar/Navbar'
 import Landing from '../Landing/Landing'
 
 export default function Header() {
   return (
     <div className='header'>
-      <Navbar />
       <Landing />
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import LastCourse from '../../components/LastCourse/LastCourse'
+import React from "react";
+import Header from "../../components/Header/Header";
+import LastCourse from "../../components/LastCourse/LastCourse";
+import Navbar from "../../components/Navbar/Navbar";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <LastCourse />
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+
+			<Header />
+			<LastCourse />
+		</div>
+	);
 }
