e5626e20fd9bffe720d95da3820c4ac2bf34cc79
Yashar <yashar22mo@gmail.com>
2024-09-15 17:30:55 +0330

    add new courses

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,11 @@
 import Home from "./pages/Home/Home";
 import Course from "./pages/Course/Course";
 import Category from "./pages/Category/Category";
-import Article from "./pages/Article/Article";
 
 
 export let routes = [
 	{ path: "/", element: <Home /> },
 	{ path: "/course/:coursename", element: <Course /> },
 	{ path: "/category/:categoryname", element: <Category /> },
-	{ path: "/article/:articlename", element: <Article /> },
 
 ];
--- src/components/LastCourse/LastCourse.jsx
+++ src/components/LastCourse/LastCourse.jsx
@@ -10,14 +10,33 @@ export default function LastCourse() {
 				<SectinHeader title="آخرین دوره ها" hasBtn={true}/>
 				<div className="row courses-content">
 					<div className="col-12 col-lg-6 col-xxl-4">
-						<CourseBox title='دوره آموزشی جنگو' img="images/courses/fareelancer.png" />
+						<CourseBox title='پروژه های فریلنسری' img="images/courses/fareelancer.png" />
 					</div>
 					<div className="col-12 col-lg-6 col-xxl-4">
-						<CourseBox title='دوره آموزشی پایتون' img="images/courses/nodejs.png" />
+						<CourseBox title='دوره API نویسی' img="images/courses/nodejs.png" />
 					</div>
 					<div className="col-12 col-lg-6 col-xxl-4">
-						<CourseBox title='دوره آموزشی نود جی اس' img="images/courses/js_project.png" />
+						<CourseBox title='پروژه های تخصصی با جاوا اسکریپت' img="images/courses/js_project.png" />
 					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره متخصص جنگو' img="images/courses/jango.png" />
+					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره آموزشی پایتون' img="images/courses/python.png" />
+					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره یوتیوبر تخصصی' img="images/courses/youtuber.png" />
+					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره متخصص نکست' img="images/courses/nextjs.webp" />
+					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره متخصص نکست' img="images/courses/python2.webp" />
+					</div>
+					<div className="col-12 col-lg-6 col-xxl-4">
+						<CourseBox title='دوره متخصص نکست' img="images/courses/pythonmini.webp" />
+					</div>
+					
 				</div>
 			</div>
 		</div>
