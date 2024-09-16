4689ab0d83b083f6ec4bd8ae5a69fd442d39ed98
Yashar <yashar22mo@gmail.com>
2024-09-16 20:39:38 +0330

    add category component logic code

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Category.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import CourseBox from "../../components/CourseBox/CourseBox";
 
 export default function Category() {
+	const [showColumn, setShowColumn] = useState(false);
+
+	const [showSelection, setShowSelection] = useState(false);
+
 	return (
 		<div className="category">
 			<Navbar />
 			<div className="container">
 				<div className="category-header">
 					<div className="category-header_filterbox">
-						<div className="category-header_row-btn">
+						<div
+							className="category-header_row-btn"
+							onClick={() => setShowColumn(!showColumn)}
+						>
 							<svg
-								class="svg-inline--fa fa-border-all"
+								className={showColumn ? "" : "svgIsActive"}
 								aria-hidden="true"
 								focusable="false"
 								data-prefix="fas"
@@ -28,9 +35,12 @@ export default function Category() {
 								></path>
 							</svg>
 						</div>
-						<div className="category-header_column-btn">
+						<div
+							className="category-header_column-btn"
+							onClick={() => setShowColumn(!showColumn)}
+						>
 							<svg
-								class="svg-inline--fa fa-align-left"
+								className={showColumn ? "svgIsActive" : ""}
 								aria-hidden="true"
 								focusable="false"
 								data-prefix="fas"
@@ -46,27 +56,48 @@ export default function Category() {
 							</svg>
 						</div>
 						<div className="category-header_selection">
-							<span className="category-header_selection-text">
+							<span
+								className="category-header_selection-text"
+								onClick={() => setShowSelection(!showSelection)}
+							>
 								مرتب سازی پیش فرض
-								<i class="fas fa-angle-down"></i>
+								<svg
+									class="svg-inline--fa fa-angle-down courses-top-bar__selection-icon"
+									aria-hidden="true"
+									focusable="false"
+									data-prefix="fas"
+									data-icon="angle-down"
+									role="img"
+									viewBox="0 0 384 512"
+									data-fa-i2svg=""
+								>
+									<path
+										fill="#fff"
+										d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"
+									></path>
+								</svg>
 							</span>
-							<ul className="category-header_selection-list">
-								<li class="category-header_selection-item">
-									مربت سازی بر اساس محبوبیت
-								</li>
-								<li class="category-header_selection-item">
-									مربت سازی بر اساس امتیاز
-								</li>
-								<li class="category-header_selection-item">
-									مربت سازی بر اساس آخرین
-								</li>
-								<li class="category-header_selection-item">
-									مربت سازی بر اساس ارزان ترین
-								</li>
-								<li class="category-header_selection-item">
-									مربت سازی بر اساس گران ترین
-								</li>
-							</ul>
+							{showSelection ? (
+								<ul className="category-header_selection-list">
+									<li class="category-header_selection-item">
+										مرتب سازی بر اساس محبوبیت
+									</li>
+									<li class="category-header_selection-item">
+										مرتب سازی بر اساس امتیاز
+									</li>
+									<li class="category-header_selection-item">
+										مرتب سازی بر اساس آخرین
+									</li>
+									<li class="category-header_selection-item">
+										مرتب سازی بر اساس ارزان ترین
+									</li>
+									<li class="category-header_selection-item">
+										مرتب سازی بر اساس گران ترین
+									</li>
+								</ul>
+							) : (
+								false
+							)}
 						</div>
 					</div>
 					<div className="category-header_searchbox">
@@ -75,25 +106,31 @@ export default function Category() {
 								className="category-header_searchbox-input"
 								placeholder="جستجوی دوره ..."
 							/>
-							<svg
-								class="svg-inline--fa fa-magnifying-glass"
-								aria-hidden="true"
-								focusable="false"
-								data-prefix="fas"
-								data-icon="magnifying-glass"
-								role="img"
-								viewBox="0 0 512 512"
-								data-fa-i2svg=""
-							>
-								<path
-									fill="#fff"
-									d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
-								></path>
-							</svg>
+							<div className="category-header_searchbox-icon">
+								<svg
+									class="svg-inline--fa fa-magnifying-glass"
+									aria-hidden="true"
+									focusable="false"
+									data-prefix="fas"
+									data-icon="magnifying-glass"
+									role="img"
+									viewBox="0 0 512 512"
+									data-fa-i2svg=""
+								>
+									<path
+										fill="#fff"
+										d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"
+									></path>
+								</svg>
+							</div>
 						</form>
 					</div>
 				</div>
-				<div class="row category-courses">
+				<div
+					class={`row ${
+						showColumn ? `category-courses-column` : `category-courses-row`
+					}`}
+				>
 					<div className="col-12 col-md-6 col-xl-4">
 						<CourseBox />
 					</div>
