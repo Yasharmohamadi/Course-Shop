85fbad959796d06bc14e3812fb5f53db29a91e93
Yashar <yashar22mo@gmail.com>
2024-09-18 20:34:17 +0330

    add accordion from react bootstrap

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
 import CourseDetailBox from "../../components/CourseDetailBox/CourseDetailBox";
+import Accordion from 'react-bootstrap/Accordion';
 import { NavLink } from "react-router-dom";
 
 export default function Course() {
@@ -192,6 +193,36 @@ export default function Course() {
 									دانلود همگانی پیوست ها
 								</a>
 							</div>
+						</div>
+						<div className="course_acoordion">
+							<Accordion defaultActiveKey="0">
+								<Accordion.Item eventKey="0" className="accordion_item">
+									<Accordion.Header>Accordion Item #1</Accordion.Header>
+									<Accordion.Body>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+										do eiusmod tempor incididunt ut labore et dolore magna
+										aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+										ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										Duis aute irure dolor in reprehenderit in voluptate velit
+										esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+										occaecat cupidatat non proident, sunt in culpa qui officia
+										deserunt mollit anim id est laborum.
+									</Accordion.Body>
+								</Accordion.Item>
+								<Accordion.Item eventKey="1">
+									<Accordion.Header>Accordion Item #2</Accordion.Header>
+									<Accordion.Body>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+										do eiusmod tempor incididunt ut labore et dolore magna
+										aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+										ullamco laboris nisi ut aliquip ex ea commodo consequat.
+										Duis aute irure dolor in reprehenderit in voluptate velit
+										esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+										occaecat cupidatat non proident, sunt in culpa qui officia
+										deserunt mollit anim id est laborum.
+									</Accordion.Body>
+								</Accordion.Item>
+							</Accordion>
 						</div>
 						<div className="course_comments">
 							<h4 className="comments_title">دیدگاهتان را بنویسید</h4>
