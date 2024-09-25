2a7b73d136ecc90c3205d59cc1758ff3c8422bb1
Yashar <yashar22mo@gmail.com>
2024-09-25 20:17:30 +0330

    coding auth context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { routes } from "./Routes";
 import { useRoutes } from "react-router-dom";
+import AuthContext from "./Context/authContext";
 
 export default function App() {
+
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [token, setToken] = useState(null)
+	const [userInfos, setUserInfos] = useState({})
+
+	const login = (token) => {
+		setToken(token)
+		localStorage.setItem('user', JSON.stringify({token}))
+		
+	}
+	const logout = () => {
+		setToken(null)
+		setUserInfos({})
+		localStorage.remove('user')
+	}	
+
 	let router = useRoutes(routes);
 	return (
-		<>
-			<div>{router}</div>
-		</>
+			<AuthContext.Provider value={{
+				isLoggedIn, // isLoggedIn: isLoggedIn,
+				token, // token: token,
+				userInfos, // userInfos: userInfos,
+				login,
+				logout: () => {},
+			}}>
+				{router}
+			</AuthContext.Provider>
+
 	);
 }
