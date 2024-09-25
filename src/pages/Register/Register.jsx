a65642eef2c055539146959785bdb0fca2a450d7
Yashar <yashar22mo@gmail.com>
2024-09-25 18:21:16 +0330

    change register inputs svg color when input is valid

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -47,7 +47,7 @@ export default function Register() {
 			password: formState.inputs.registerPassword.value,
 			confirmPassword: formState.inputs.registerPassword.value,
 		};
-		// console.log(newUserInfos);
+		console.log(newUserInfos);
 
 		// fetch("http://localhost:3000/v1/auth/register", {
 		// 	method: "POST",
@@ -82,7 +82,7 @@ export default function Register() {
 							></Input>
 							<svg viewBox="0 0 576 512">
 								<path
-									fill="#ffffff41"
+									fill={formState.inputs.registerFullName.isValid ? '#00e000b4' : '#ffffff41'}
 									d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm80 256l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16L80 384c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
 								/>
 							</svg>
@@ -113,7 +113,7 @@ export default function Register() {
 								data-fa-i2svg=""
 							>
 								<path
-									fill="#ffffff41"
+									fill={formState.inputs.registerUsername.isValid ? '#00e000b4' : '#ffffff41'}
 									d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
 								></path>
 							</svg>{" "}
@@ -139,7 +139,7 @@ export default function Register() {
 								data-fa-i2svg=""
 							>
 								<path
-									fill="#ffffff41"
+									fill={formState.inputs.registerEmail.isValid ? '#00e000b4' : '#ffffff41'}
 									d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"
 								></path>
 							</svg>{" "}
@@ -169,7 +169,7 @@ export default function Register() {
 								data-fa-i2svg=""
 							>
 								<path
-									fill="#ffffff41"
+									fill={formState.inputs.registerPassword.isValid ? '#00e000b4' : '#ffffff41'}
 									d="M352 192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H288V144C288 64.47 352.5 0 432 0C511.5 0 576 64.47 576 144V192C576 209.7 561.7 224 544 224C526.3 224 512 209.7 512 192V144C512 99.82 476.2 64 432 64C387.8 64 352 99.82 352 144V192z"
 								></path>
 							</svg>{" "}
@@ -177,7 +177,7 @@ export default function Register() {
 						<Button
 							className="form_btn"
 							event={registerNewUser}
-							// isDisabled={!formState.isFormValid}
+							isDisabled={!formState.isFormValid}
 						>
 							ثبت نام
 							<svg
