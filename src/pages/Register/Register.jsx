e9ea7b2f259d4beeded57be5d7c412d0e5b38fb7
Yashar <yashar22mo@gmail.com>
2024-09-25 17:26:33 +0330

    add a new input for fullname in register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -16,6 +16,10 @@ import { useForm } from "../../hooks/useForm";
 export default function Register() {
 	const [formState, onInputHandler] = useForm(
 		{
+			registerFullName: {
+				value: "",
+				isValid: false,
+			},
 			registerUsername: {
 				value: "",
 				isValid: false,
@@ -33,6 +37,12 @@ export default function Register() {
 	);
 	// console.log("formState in register: ", formState);
 
+	const registerNewUser = (event) => {
+		event.preventDefault();
+		
+		
+	};
+
 	return (
 		<div className="register">
 			<Navbar />
@@ -43,6 +53,24 @@ export default function Register() {
 						<div className="form_login_route">
 							<span>قبلا ثبت نام کردی؟</span>
 							<Link to="/login">ورود</Link>
+						</div>
+						<div className="input_wrapper">
+							<Input
+								className="form_input"
+								id="registerFullName"
+								maxLength="60"
+								type="text"
+								placeholder="نام و نام خانوادگی"
+								element="input"
+								validation={[requiredValidator(), minValidator(6)]}
+								onInputHandler={onInputHandler}
+							></Input>
+							<svg viewBox="0 0 576 512">
+								<path
+									fill="#ffffff41"
+									d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm80 256l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16L80 384c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
+								/>
+							</svg>
 						</div>
 						<div className="input_wrapper">
 							<Input
@@ -131,7 +159,11 @@ export default function Register() {
 								></path>
 							</svg>{" "}
 						</div>
-						<Button className="form_btn" onClick={false} isDisabled={!formState.isFormValid}>
+						<Button
+							className="form_btn"
+							event={registerNewUser}
+							isDisabled={!formState.isFormValid}
+						>
 							ثبت نام
 							<svg
 								aria-hidden="true"
