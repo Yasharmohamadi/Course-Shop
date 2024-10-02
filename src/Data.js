c7c0108e316a191fc79119a43bee37be26c88e73
Yashar <yashar22mo@gmail.com>
2024-10-02 16:14:35 +0330

    add data.js for our datas

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const coursesArray = [
+    {
+        id: 1,
+        title: "دوره شماره یک",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 2,
+        title: "دوره شماره دو",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 3,
+        title: "دوره شماره سه",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 4,
+        title: "دوره شماره چهار",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 5,
+        title: "دوره شماره پنج",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 6,
+        title: "دوره شماره شش",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 7,
+        title: "دوره شماره هفت",
+        img: "/images/courses/course-cover.png",
+    },
+    {
+        id: 8,
+        title: "دوره شماره هشت",
+        img: "/images/courses/course-cover.png",
+    },
+]
--- src/components/LastCourse/LastCourse.jsx
+++ src/components/LastCourse/LastCourse.jsx
@@ -3,50 +3,10 @@ import "./LastCourse.css";
 import SectinHeader from "../SectinHeader/SectinHeader";
 import CourseBox from "../CourseBox/CourseBox";
 import { Link } from "react-router-dom";
+import { coursesArray } from "../../Data";
 
 export default function LastCourse() {
-	const [courses, setCourses] = useState([
-		{
-			id: 1,
-			title: "دوره شماره یک",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 2,
-			title: "دوره شماره دو",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 3,
-			title: "دوره شماره سه",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 4,
-			title: "دوره شماره چهار",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 5,
-			title: "دوره شماره پنج",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 6,
-			title: "دوره شماره شش",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 7,
-			title: "دوره شماره هفت",
-			img: "/images/courses/course-cover.png",
-		},
-		{
-			id: 8,
-			title: "دوره شماره هشت",
-			img: "/images/courses/course-cover.png",
-		},
-	]);
+	const [courses, setCourses] = useState(coursesArray);
 	return (
 		<div className="last-course">
 			<div className="container">
