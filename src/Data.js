40c036c1e544e3094d77e954bd4c0970b0048330
Yashar <yashar22mo@gmail.com>
2024-10-02 19:20:54 +0330

    change theme

--- src/Data.js
+++ src/Data.js
@@ -2,41 +2,41 @@ export const coursesArray = [
     {
         id: 1,
         title: "دوره شماره یک",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 2,
         title: "دوره شماره دو",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 3,
         title: "دوره شماره سه",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 4,
         title: "دوره شماره چهار",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 5,
         title: "دوره شماره پنج",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 6,
         title: "دوره شماره شش",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 7,
         title: "دوره شماره هفت",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
     {
         id: 8,
         title: "دوره شماره هشت",
-        img: "/images/courses/course-cover.png",
+        img: "/images/background_landing.jfif",
     },
 ]
